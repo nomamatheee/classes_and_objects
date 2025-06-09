fb3a0f58ef821172a336afa9bba0e4c4904db046
nomamatheee <157741578+nomamatheee@users.noreply.github.com>
2025-06-09 22:35:13 +0200

    Introduction to classes and objects
    The project defines a simple Student class that stores information about a student, such as their name, age, and course. It uses a constructor to initialize the values, and a method called DisplayInfo() to print the details of the student.
    
    This project helps beginners understand how to:
    
    Create and use classes in C#
    
    Define and initialize fields using a constructor
    
    Call methods on an object instance
    
    Work with multiple object instances

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student one = new Student("noma", "IT", 23);
+            one.DisplayInfo();
+
+            Student two = new Student("thami", "biology", 28);
+            two.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatExercise
+{
+     class Student
+    {
+        //fields
+        public string name,course;
+        public int age;
+        
+
+        //constructor
+         public Student(string name,string course,int age)
+        {
+            this.name = name;
+            this.course = course;
+            this.age = age;
+        }
+
+        //method
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {name} ");
+            Console.WriteLine($"Course: {course}");
+            Console.WriteLine($"Age:{age}");
+        }
+
+
+
+
+    }
+}
